bc97195b425ebfbb45f5559584d359348bdc1d49
samoye <meetsamueloyewole@.com>
2021-05-19 14:49:36 +0100

    models

--- samuelscrumy/admin.py
+++ samuelscrumy/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 
-# # Register your models here.
+# Register your models here.
 
-# admin.site.register(ScrumyGoals)
-# admin.site.register(ScrumyHistory)
-# admin.site.register(GoalStatus)
+admin.site.register(ScrumyGoals)
+admin.site.register(ScrumyHistory)
+admin.site.register(GoalStatus)
