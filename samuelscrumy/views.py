62f988355bd3165f37084735b3a04a7eb1232f3d
samoye <meetsamueloyewole@.com>
2021-05-20 12:36:51 +0100

    views and urls update

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from .models import *
 
 
 def index(request): 
-    return HttpResponse("This is a Scrum Application") 
+    goal = ScrumyGoals.objects.filter(goal_name="Learn Django")
+    return HttpResponse(goal) 
+
+def move_goal(request, goal_id):
+  item = ScrumyGoals.objects.get(goal_id=goal_id)
+  print(item)
+  return HttpResponse(item.goal_name) 
