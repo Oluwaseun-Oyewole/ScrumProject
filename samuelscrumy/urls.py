c72a8964b101464dab9cbbdff5c5d3572892fa20
samoye <meetsamueloyewole@.com>
2021-05-20 17:52:42 +0100

    urls,py

--- samuelscrumy/urls.py
+++ samuelscrumy/urls.py
@@ -3,9 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('movegoal/<int:goal_id>/', views.move_goal),
-    path('addgoal', views.add_goal),
+    path('addgoal/', views.add_goal),
     path('home/', views.home)
 ]
-
-
- 
