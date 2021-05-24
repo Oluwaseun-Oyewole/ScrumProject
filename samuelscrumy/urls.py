ffed4bbdbfc20cd1f9ec73b787936b0da77268d7
samoye <meetsamueloyewole@.com>
2021-05-24 14:17:21 +0100

    creating views that access the database

--- samuelscrumy/urls.py
+++ samuelscrumy/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    # path('movegoal/<int:goal_id>', views.move_goal),
-    # path('addgoal/', views.add_goal),
-    # path('home/', views.home)
+    path('movegoal/<int:goal_id>', views.move_goal),
+    path('addgoal/', views.add_goal),
+    path('home/', views.home)
 ]
--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -12,23 +12,23 @@ def index(request):
   goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
   return HttpResponse(goal_name) 
 
-# def move_goal(request, goal_id):
-#   goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
-#   return HttpResponse(goal_item.goal_name) 
+def move_goal(request, goal_id):
+  goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
+  return HttpResponse(goal_item.goal_name) 
 
-# def add_goal(request):
-#   ScrumyGoals.objects.create(
-#     goal_name = 'Keep Learning Django',
-#     goal_id = random.randint(1000, 9999),
-#     created_by = 'Louis',
-#     moved_by = 'Louis',
-#     owner = 'Louis',
-#     goal_status = GoalStatus.objects.get(id=1),
-#     user = User.objects.get(username='Louis') 
-#   )
-#   return HttpResponse('new item created')
+def add_goal(request):
+  ScrumyGoals.objects.create(
+    goal_name = 'Keep Learning Django',
+    goal_id = random.randint(1000, 9999),
+    created_by = 'Louis',
+    moved_by = 'Louis',
+    owner = 'Louis',
+    goal_status = GoalStatus.objects.get(id=1),
+    user = User.objects.get(username='Louis') 
+  )
+  return HttpResponse('new item created')
 
-# def home(request):
-  # items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
-  # output = ' '.join([eachgoal.goal_name for eachgoal in items])
-  # return HttpResponse("The goal name " + output + " should be displayed on the webpage")
+def home(request):
+  items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
+  output = ' '.join([eachgoal.goal_name for eachgoal in items])
+  return HttpResponse("The goal name " + output + " should be displayed on the webpage")
