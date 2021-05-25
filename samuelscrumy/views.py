b92d4ccf9c370fdec95e42d86c97312632c7c500
samoye <meetsamueloyewole@.com>
2021-05-25 16:24:11 +0100

    setting django template index

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -5,9 +5,9 @@ import random
 from django.contrib.auth.models  import User
 
     
-def index(request): 
-  goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
-  return HttpResponse(goal_name) 
+# def index(request): 
+#   goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
+#   return HttpResponse(goal_name) 
 
 def move_goal(request, goal_id):
   goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
@@ -30,6 +30,6 @@ def home(request):
   output = ' '.join([eachgoal.goal_name for eachgoal in items])
   return HttpResponse("The goal name " + output + " should be displayed on the webpage")
     
-# def index(request):
-#   return render(request, 'samuelscrumy/home.html', {"goal_name":"Keep Learning Django", "goal_id": 2, "user":User.objects.get(username='louis')})
+def index(request):
+  return render(request, 'samuelscrumy/home.html', {"goal_name":"Keep Learning Django", "goal_id": 2, "user":User.objects.get(username='louis')})
 
