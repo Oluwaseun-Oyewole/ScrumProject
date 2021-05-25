aaa7e1549858fbed308cac2c927af4aa5c95b286
samoye <meetsamueloyewole@.com>
2021-05-25 14:16:15 +0100

    django template

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -9,18 +9,11 @@ def index(request):
   goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
   return HttpResponse(goal_name) 
 
-# def move_goal(request, goal_id):
-#   goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
-#   return HttpResponse(goal_item.goal_name) 
-
 def move_goal(request, goal_id):
-    try:
-      obj = ScrumyGoals.objects.get(goal_id=goal_id)
-    except Exception as e: 
-      return render(request, 'samuelscrumy/exception.html', {'error': 'A record with that goal id does not exist'}) 
-    else: return HttpResponse(obj.goal_name)
+  goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
+  return HttpResponse(goal_item.goal_name) 
       
-      
+
 def add_goal(request):
   ScrumyGoals.objects.create(
     goal_name = 'Keep Learning Django',
@@ -33,18 +26,18 @@ def add_goal(request):
   )
   return HttpResponse('new item created')
 
-# def home(request):  
-#   items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
-#   output = ' '.join([eachgoal.goal_name for eachgoal in items])
-#   return HttpResponse("The goal name " + output + " should be displayed on the webpage")
+def home(request):  
+  items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
+  output = ' '.join([eachgoal.goal_name for eachgoal in items])
+  return HttpResponse("The goal name " + output + " should be displayed on the webpage")
 
 
     
-def home(request):
-  goal_items = {
-    "goal_name":"Learn Django",
-    "goal_id": 2,
-    "user":User.objects.get(username='louis')
-  }
-  return render(request, 'samuelscrumy/home.html', context={"goal_items":goal_items})
+# def index(request):
+#   goal_items = {
+#     "goal_name":"Learn Django",
+#     "goal_id": 2,
+#     "user":User.objects.get(username='louis')
+#   }
+#   return render(request, 'samuelscrumy/home.html', context={"goal_items":goal_items})
 
