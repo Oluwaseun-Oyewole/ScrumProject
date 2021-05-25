906c05ca9451c3d21f2f75bc8f14d05bd9f5ef37
samoye <meetsamueloyewole@.com>
2021-05-25 14:47:49 +0100

    setting django template home view

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -5,9 +5,9 @@ import random
 from django.contrib.auth.models  import User
 
     
-# def index(request): 
-#   goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
-#   return HttpResponse(goal_name) 
+def index(request): 
+  goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
+  return HttpResponse(goal_name) 
 
 def move_goal(request, goal_id):
   goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
@@ -26,13 +26,13 @@ def add_goal(request):
   )
   return HttpResponse('new item created')
 
-def home(request):  
-  items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
-  output = ' '.join([eachgoal.goal_name for eachgoal in items])
-  return HttpResponse("The goal name " + output + " should be displayed on the webpage")
+# def home(request):  
+#   items = ScrumyGoals.objects.filter(goal_name="Keep Learning Django")
+#   output = ' '.join([eachgoal.goal_name for eachgoal in items])
+#   return HttpResponse("The goal name " + output + " should be displayed on the webpage")
 
 
     
-def index(request):
-  return render(request, 'samuelscrumy/home.html', {"goal_name":"Learn Django", "goal_id": 2, "user":User.objects.get(username='louis')})
+def home(request):
+  return render(request, 'samuelscrumy/home.html', {"goal_name":"Keep Learning Django", "goal_id": 2, "user":User.objects.get(username='louis')})
 
