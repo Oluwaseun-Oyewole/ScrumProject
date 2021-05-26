961e4c03fa74e492d1b05043437f984626c18efa
samoye <meetsamueloyewole@.com>
2021-05-26 10:55:56 +0100

    creating views that accept arguments

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -5,20 +5,13 @@ import random
 from django.contrib.auth.models  import User
 
     
-# def index(request): 
-#   goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
-#   return HttpResponse(goal_name) 
-
-# def move_goal(request, goal_id):
-#   goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
-#   return HttpResponse(goal_item.goal_name) 
+def index(request): 
+  goal_name = ScrumyGoals.objects.filter(goal_name="Learn Django")
+  return HttpResponse(goal_name) 
 
 def move_goal(request, goal_id):
-  try:
-      obj = ScrumyGoals.objects.get(goal_id=goal_id)
-  except Exception as e:
-    return render(request, 'samuelscrumy/exception.html', {'error': 'A record with that goal id does not exist'})
-  return HttpResponse(obj.goal_name)
+  goal_item = ScrumyGoals.objects.get(goal_id=goal_id)
+  return HttpResponse(goal_item.goal_name) 
 
 def add_goal(request):
   ScrumyGoals.objects.create(
@@ -37,6 +30,3 @@ def home(request):
   output = ' '.join([eachgoal.goal_name for eachgoal in items])
   return HttpResponse("The goal name " + output + " should be displayed on the webpage")
     
-def index(request):
-  return render(request, 'samuelscrumy/home.html', {"goal_name":"Keep Learning Django", "goal_id": 2, "user":User.objects.get(username='louis')})
-
