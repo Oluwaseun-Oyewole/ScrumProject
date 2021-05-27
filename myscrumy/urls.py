324c1f93b69e1b7a39570f609ce643e50952213f
samoye <meetsamueloyewole@.com>
2021-05-27 11:40:44 +0100

    url namespacing

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('samuelscrumy/', include('samuelscrumy.urls'))
+    path('samuelscrumy/', include('samuelscrumy.urls')),
+    
 ]
--- samuelscrumy/urls.py
+++ samuelscrumy/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     path('movegoal/<int:goal_id>/', views.move_goal),
     path('addgoal/', views.add_goal),
-    path('home/', views.home)
+    path('home/', views.home),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
