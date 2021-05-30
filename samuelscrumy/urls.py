379c2b294c27ecbde20dc691b90c37080be3af4d
samoye <meetsamueloyewole@.com>
2021-05-30 20:15:41 +0100

    form handling

--- samuelscrumy/urls.py
+++ samuelscrumy/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name="about",),
     path('movegoal/<int:goal_id>/', views.move_goal),
     path('addgoal/', views.add_goal, name="addgoal"),
-    path('home/', views.home),
+    path('home/', views.home, name="account"),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
