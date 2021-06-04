c481cc0bf59ce3c1bf7f566f035fbca71dd023aa
samoye <meetsamueloyewole@.com>
2021-06-04 16:46:15 +0100

    changes

--- samuelscrumy/forms.py
+++ samuelscrumy/forms.py
@@ -27,7 +27,7 @@ class SelectGroupForm(forms.ModelForm):
 class CreateGoalForm(forms.ModelForm):
     class Meta:
         model = ScrumyGoals
-        fields = ['goal_name', 'user']
+        fields = ['goal_name']
 
 
 class UpdateGoalForm(forms.ModelForm):
