582dbd27e09ffa2a15492790213e00c7ce30a339
SAMUEL OYEWOLE <meetsamueloyewole@gmail.com>
2021-06-06 05:10:56 +0100

    database configurations

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'linuxjob',
-        # 'USER':'seun',
-        # 'PASSWORD':'09028471934githubGit@',
-        # 'HOST':'localhost',
-        # 'PORT':'3306'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-1',
+        'USER':'seun',
+        'PASSWORD':'websocketiotesting',
+        'HOST':'database-1.cemhpmkut0vz.us-east-1.rds.amazonaws.com',
+        'PORT':'3306'
     }
 }
 
