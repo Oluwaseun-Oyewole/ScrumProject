b598ff886b5bdbce91b4a3529f62e9c758a86b71
SAMUEL OYEWOLE <meetsamueloyewole@gmail.com>
2021-06-16 11:59:48 +0100

    readme

--- samuelscrumy/views.py
+++ samuelscrumy/views.py
@@ -11,24 +11,23 @@ from django.http import HttpResponseForbidden
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
+# from .forms import DeveloperCreateGoalForm, QACreateGoalForm
 
 
-def is_developer(user):
-    return user.groups.filter(name='Developer').exists()
+def home(request):
+      
+  users = User.objects.all()
+  weekly = ScrumyGoals.objects.filter(goal_status=1)
+  Daily = ScrumyGoals.objects.filter(goal_status=2)
+  Verify = ScrumyGoals.objects.filter(goal_status=3)
+  Done = ScrumyGoals.objects.filter(goal_status=4)
   
-@user_passes_test(is_developer)
-def test_group(request):
-      print(request.user.groups.all())
-      all = request.user.groups.all()
-      for item in all:
-            if item.name == 'Developer':
-                  print("good")
-            else:
-                  print("bad")
-      print('yes')
-      return HttpResponse("hello")
-    
-    
+  
+  context = {'users':users, 'weekly':weekly, 'Daily':Daily, 'Verify':Verify, 'Done':Done,  }      
+  return render(request, 'samuelscrumy/home.html', context)
+
+
+# login page for users
 def index(request): 
   if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -44,9 +43,111 @@ def index(request):
     form = SignupForm()
   return render(request, 'samuelscrumy/index.html', {'form': form})
   
+  
+#   for successfull login
 def creation(request):
   return render(request, 'samuelscrumy/creation.html')
   
+  
+
+def add_goal(request):
+      all = request.user.groups.all()
+      if request.method == 'POST':
+            form = CreateGoalForm(request.POST)
+            if form.is_valid():
+                  user = form.save(commit=False)
+                  user.goal_status_id = 1
+                  all = request.user.groups.all()
+                  for item in all:
+                        if item.name != 'owner':
+                              user.user = User.objects.get(username=request.user.username)
+                              user.save()
+                        else:
+                              user.save()                  
+                  return redirect('/samuelscrumy/home')
+      else:
+            form = CreateGoalForm()
+      return render(request, 'samuelscrumy/addgoal.html', {'form': form})
+
+# note: remember to create a form form the dev, QA
+# def add_goal(request):
+#       dev_g = Group.objects.get(id=1)
+#       dev_g = User.objects.filter(groups__name_in=["Developer"])
+      
+#       admin_g = Group.objects.get(id=2)
+#       admin_g = User.objects.filter(groups__name_in=["Admin"])
+      
+#       qa_g = Group.objects.get(id=3)
+#       qa_g = User.objects.filter(groups__name_in=["Quality-Assurance"])
+      
+#       owner_g = Group.objects.get(id=4)
+#       owner_g = User.objects.filter(groups__name_in=["Owner"])
+      
+#       if request.user in dev_g:
+#             if request.method == 'POST':
+#                   form = DeveloperCreateGoalForm(request.POST)
+#                   if form.is_valid():
+#                         user = form.save(commit=False)
+#                         user.goal_status_id = 1
+#                         user.save()
+                        
+#                         return redirect  ("/samuelscrumy/home")       
+#                   return HttpResponse("Invalid credentials provided, please fill out all fields")
+#             else:
+#                   form = DeveloperCreateGoalForm()
+#                   return render (request, 'samuelscrumy/addgoal.html',{'form':form})
+            
+#       elif request.user in qa_g:
+#             if request.method =='POST':
+#                   form = QACreateGoalForm(request.POST)
+#                   if form.is_valid():
+#                         user = form.save(commit=False)
+#                         user.goal_status_id = 1
+#                         user.save()
+#                         return redirect  ("/samuelscrumy/home")       
+#                   return HttpResponse("Invalid credentials provided, please fill out all fields")
+#             else:
+#                   form = QACreateGoalForm()
+#                   return render(request, 'samuelscrumy/addgoal.html', {"form":form})
+                    
+#       elif request.user in admin_g:
+#         if request.method =='POST':
+#             form = CreateGoalForm(request.POST)
+#             if form.is_valid():
+#                    form.save()
+#                    return redirect  ("/samuelscrumy/home")       
+#             return HttpResponse("Invalid credentials provided, please fill out all fields")
+#         else:
+#             form = CreateGoalForm()
+#             return render (request, 'samuelscrumy/addgoal.html', {"form":form})
+      
+#       elif request.user in owner_g:
+#         if request.method =='POST':
+#             form = CreateGoalForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect  ("/samuelscrumy/home")       
+#             return HttpResponse("Invalid credentials provided, please fill out all fields")
+#         else:
+#             form = CreateGoalForm()
+    
+#         return render (request, 'samuelscrumy/addgoal.html', {"form":form})
+            
+            
+# def is_developer(user):
+#     return user.groups.filter(name='Developer').exists()
+  
+# @user_passes_test(is_developer)
+# def test_group(request):
+#       print(request.user.groups.all())
+#       all = request.user.groups.all()
+#       for item in all:
+#             if item.name == 'Developer':
+#                   print("good")
+#             else:
+#                   print("bad")
+#       print('yes')
+#       return HttpResponse("hello")
 
 
 def move_goal(request, goal_id): 
@@ -90,37 +191,6 @@ def move_goal(request, goal_id):
       # else:
       #     return render(request, 'samuelscrumy/exception.html')
 
-
-def add_goal(request):
-      all = request.user.groups.all()
-      if request.method == 'POST':
-            form = CreateGoalForm(request.POST)
-            if form.is_valid():
-                  user = form.save(commit=False)
-                  user.goal_status_id = 1
-                  all = request.user.groups.all()
-                  for item in all:
-                        if item.name != 'owner':
-                              user.user = User.objects.get(username=request.user.username)
-                              user.save()
-                        else:
-                              user.save()                  
-                  return redirect('/samuelscrumy/home')
-      else:
-            form = CreateGoalForm()
-      return render(request, 'samuelscrumy/addgoal.html', {'form': form})
-
-
-def home(request):
-  users = User.objects.all()
-  weekly = ScrumyGoals.objects.filter(goal_status=1)
-  Daily = ScrumyGoals.objects.filter(goal_status=2)
-  Verify = ScrumyGoals.objects.filter(goal_status=3)
-  Done = ScrumyGoals.objects.filter(goal_status=4)
-  context = {'users':users, 'weekly':weekly, 'Daily':Daily, 'Verify':Verify, 'Done':Done,  }      
-  return render(request, 'samuelscrumy/home.html', context)
-
-      
       
 def add_group(request, id):
       
